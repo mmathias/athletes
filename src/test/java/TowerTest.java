0d6931defed26d1a479d0283d1a15faf3b077367
Magno Mathias <magnomathias21@gmail.com>
2020-02-20 01:10:59 +0100

    organised tests

--- src/test/java/TowerTest.java
+++ src/test/java/TowerTest.java
@@ -1,4 +1,3 @@
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -89,35 +88,5 @@ public class TowerTest {
 
         int maxAthletesInPyramid = tower.getMaxAthletes();
         assertEquals(1, maxAthletesInPyramid);
-    }
-
-    @Test
-    @Ignore // esse teste nao eh valido
-    // o enunciado diz que se m[i] > m[j], entao s[i] > s[j]
-    // ou seja, o quarto atleta, comparado ao terceiro, tem m[3] = 8 > m[2] = 7, porem s[3] = 6 < s[2] = 20,
-    // o que faz com que o teste nao seja aderente ao enunciado
-    public void itShouldReturn3WhenTheHeaviestIsNotTheStrongestButTheSecondHeaviestIs() {
-        List<Athlete> athletes = new ArrayList<>();
-        athletes.add(new Athlete(5, 1));
-        athletes.add(new Athlete(6, 2));
-        athletes.add(new Athlete(7, 20));
-        athletes.add(new Athlete(8, 6));
-        Tower tower = new Tower(athletes);
-
-        int maxAthletesInPyramid = tower.getMaxAthletes();
-        assertEquals(3, maxAthletesInPyramid);
-    }
-
-    @Test
-    public void acceptanceCriterion() {
-        List<Athlete> athletes = new ArrayList<>();
-        athletes.add(new Athlete(3, 4));
-        athletes.add(new Athlete(2, 2));
-        athletes.add(new Athlete(7, 6));
-        athletes.add(new Athlete(4, 5));
-
-        Tower tower = new Tower(athletes);
-
-        assertEquals(3, tower.getMaxAthletes());
     }
 }
